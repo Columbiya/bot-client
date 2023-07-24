c0b7dd688a960cfc741c6ddd8a1286711d8251c0
Columbiya <volodiabykov216@gmail.com>
2023-07-24 20:56:04 +0500

    added ballac notification

--- src/clients/DiscordBotClient/index.ts
+++ src/clients/DiscordBotClient/index.ts
@@ -1,37 +1,60 @@
+import { EventEmitter } from "node:events"
 import { BotClient } from "@/interfaces"
 import { GatewayDispatchEvents } from "@discordjs/core"
-import { Haman, Dragon } from "@/models"
+import { Haman, Dragon, Ballac } from "@/models"
 import { FileService } from "@/helpers"
 import { AppearableEntityWatcher } from "@/features"
+import { lunarVisionNotificationsChannelId } from "./lunarVisionNotificationsChannelId"
+import { EventTypes } from "@/types"
 
 export class DiscordBotClient extends BotClient {
+  emitter = new EventEmitter()
+
   setupListeners(): void {
-    this.once(GatewayDispatchEvents.Ready, async () => {
+    this.once(GatewayDispatchEvents.Ready, () => {
       console.log("ready")
     })
+
+    this.emitter.on(EventTypes.SEND_MESSAGE, this.sendNotification.bind(this))
   }
 
   async setupHooks() {
     const haman = new Haman(new FileService())
-    await haman.fetchAppearTime()
+    haman.fetchAppearTime()
 
     const threshholdMinutes = 10
     const hamanAppearTimeWatcher = new AppearableEntityWatcher(
       haman,
-      threshholdMinutes,
-      this
+      this.emitter,
+      threshholdMinutes
     )
 
     const dragon = new Dragon(new FileService())
-    await dragon.fetchAppearTime()
+    dragon.fetchAppearTime()
 
     const dragonAppearTimeWatcher = new AppearableEntityWatcher(
       dragon,
-      threshholdMinutes,
-      this
+      this.emitter,
+      threshholdMinutes
+    )
+
+    const ballac = new Ballac(new FileService())
+    ballac.fetchAppearTime()
+
+    const ballacAppearTimeWatcher = new AppearableEntityWatcher(
+      ballac,
+      this.emitter,
+      threshholdMinutes
     )
 
     hamanAppearTimeWatcher.startWatching()
     dragonAppearTimeWatcher.startWatching()
+    ballacAppearTimeWatcher.startWatching()
+  }
+
+  private sendNotification(content: string) {
+    return this.api.channels.createMessage(lunarVisionNotificationsChannelId, {
+      content,
+    })
   }
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export { Client } from "@/types/Client"
 export { DaysOfWeek } from "@/types/DaysOfWeek"
 export { AppearTime } from "@/types/AppearTime"
+export { EventTypes } from "@/types/EventTypes"
